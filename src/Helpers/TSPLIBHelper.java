550642327d4cc55cb278f482ecb71c48fc0da0ca
Facundo Rodríguez <facundo.rodriguez@deephow.com>
2022-04-30 12:43:19 -0300

    Added ability to read TSP file data and process into paths and cities

--- src/Helpers/TSPLIBHelper.java
+++ src/Helpers/TSPLIBHelper.java
@@ -8,13 +8,20 @@ import java.util.Scanner;
 
 public class TSPLIBHelper {
 
-	ArrayList<Integer> cities = new ArrayList<Integer>();
-
+	private HashMap<Integer, HashMap<Integer, Integer>> paths;
+	private HashMap<Integer, Integer> cities = new HashMap<Integer, Integer>();
+	private final Integer DIAG_VALUE = 9999;
 	private TSPInstance instanceData;
 
 	public TSPLIBHelper() {
+		this.paths = new HashMap<Integer, HashMap<Integer, Integer>>();
 	}
 
+	/**
+	 * Read and process file given an fileUrl
+	 * 
+	 * @param {String} fileUrl
+	 */
 	public void readTSPInstanceFile(String fileUrl) {
 		try {
 			File file = new File(fileUrl);
@@ -25,6 +32,9 @@ public class TSPLIBHelper {
 				String[] row = data.split(":");
 				this.processRow(row);
 			}
+			ArrayList<Integer> cities = new ArrayList<Integer>(this.cities.values());
+			this.instanceData.setCities(cities);
+			this.instanceData.setPaths(paths);
 			reader.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
@@ -32,6 +42,11 @@ public class TSPLIBHelper {
 		}
 	}
 
+	/**
+	 * Process each row file
+	 * 
+	 * @param {String[]} row
+	 */
 	private void processRow(String[] row) {
 		String name = "";
 		Integer dimension = 1;
@@ -55,20 +70,88 @@ public class TSPLIBHelper {
 			String auxRow = row[0].replaceAll("\\s", "-");
 			if (!auxRow.equals("EDGE_WEIGHT_SECTION") && !auxRow.equals("EOF")) {
 				String splitRow[] = auxRow.split("-");
-				for (String city : splitRow) {
-					if (!city.equals("")) {
-						Integer cityToAdd = Integer.parseInt(city.trim());
-						if (!cities.contains(cityToAdd)) {
-							this.cities.add(cityToAdd);
-							System.out.println("city: " + cityToAdd);
-						}
-							
-					}
-
-				}
+				if (this.cities.size() == 0)
+					this.createCities(splitRow);
+				else
+					this.createPaths(splitRow);
 
 			}
 		}
+	}
+
+	/**
+	 * Create cities given the first row of file
+	 * 
+	 * @param {String[]} citiesRow
+	 */
+	private void createCities(String[] citiesRow) {
+		Integer indexCityToAdd = 1;
+		for (int i = 0; i < citiesRow.length; i++) {
+			String city = citiesRow[i];
+			if (!city.equals("")) {
+				Integer cityToAdd = Integer.parseInt(citiesRow[i].trim());
+				if (!cities.containsValue(cityToAdd) && !cityToAdd.equals(this.DIAG_VALUE))
+					this.cities.put(indexCityToAdd, cityToAdd);
+				indexCityToAdd++;
+			}
+		}
+
+	}
+
+	/**
+	 * Create paths give a row file
+	 * 
+	 * @param {String[]} citiesRow
+	 */
+	private void createPaths(String[] pathRow) {
+		Integer cityIndex = 1;
+		Integer city = Integer.parseInt(pathRow[getFirstCity(pathRow)]);
+		for (int i = 0; i < pathRow.length; i++) {
+			String path = pathRow[i];
+			if (!path.equals("")) {
+				Integer cityToAdd = this.cities.get(cityIndex);
+				Integer costPath = Integer.parseInt(path.trim());
+				if (cityToAdd != null ) {
+					if (this.paths.containsKey(city)) {
+						HashMap<Integer, Integer> pathCity = this.paths.get(city);
+						if (!pathCity.containsKey(cityToAdd))
+							pathCity.put(cityToAdd, costPath);
+					} else {
+						if (!this.paths.containsKey(city)) {
+							HashMap<Integer, Integer> newPath = new HashMap<Integer, Integer>();
+							newPath.put(cityToAdd, costPath);
+							this.paths.put(city, newPath);
+						}
+					}
+				}
+				cityIndex++;
+			}
+		}
+	}
+
+	/**
+	 * Get the first city different to ""
+	 * 
+	 * @param {String[]} row
+	 * @return {Integer} the first city of row
+	 */
+	private Integer getFirstCity(String[] row) {
+		String value = row[0];
+		int i = 0;
+		while (value.equals("") && i < row.length) {
+			value = row[i].trim();
+			i++;
+		}
+		if (i <= row.length)
+			return i - 1;
+		return -1;
+	}
+	
+	public TSPInstance getTSPInstance() {
+		if (this.instanceData != null) {
+			return this.instanceData;
+		}
+		return null;
 	}
 
 }
